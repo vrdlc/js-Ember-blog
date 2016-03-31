9132cd85e5624b24158f0dbe6b1983e8aa854a53
Chris Young and Ramon de la Cruz <ctopheryoung@gmail.com>
2016-03-31 15:02:57 -0700

    use xselect to implement dropdowns for relational data

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -2,17 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-  savePost () {
-    var params = {
-      title: this.get('title') ? this.get('title') : "",
-      date: this.get('date') ? this.get('date') : "",
-      body: this.get('body') ? this.get('body') : "",
-      link: this.get('link') ? this.get('link') : "",
-      screenshot: this.get('screenshot') ? this.get('screenshot') : "",
-      category: this.get('category')
-    };
-    console.log(params.category);
-    this.sendAction('savePost', params);
+    savePost () {
+      var params = {
+        title: this.get('title') ? this.get('title') : "",
+        date: this.get('date') ? this.get('date') : "",
+        body: this.get('body') ? this.get('body') : "",
+        link: this.get('link') ? this.get('link') : "",
+        screenshot: this.get('screenshot') ? this.get('screenshot') : "",
+        category: this.get('category')
+      };
+      console.log(params.category);
+      this.sendAction('savePost', params);
+    }
   }
-}
 });
